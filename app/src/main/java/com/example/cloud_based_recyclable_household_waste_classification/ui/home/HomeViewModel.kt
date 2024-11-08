ec42ea910ccdbfb31082116b6bfee408dff19ce5
alvinsaputra123 <alvin.saputra20162016@gmail.com>
2024-11-08 14:24:26 +0700

    Initial commit

---
+++ app/src/main/java/com/example/cloud_based_recyclable_household_waste_classification/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.cloud_based_recyclable_household_waste_classification.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/com/example/cloud_based_recyclable_household_waste_classification/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.cloud_based_recyclable_household_waste_classification.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
