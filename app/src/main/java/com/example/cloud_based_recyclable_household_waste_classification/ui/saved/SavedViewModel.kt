dac4328f5506fffd4fdf8cfd84293002be10f256
alvinsaputra123 <alvin.saputra20162016@gmail.com>
2024-11-09 14:16:14 +0700

    1. Mengubah nama direktori default Dashobard -> Saved, termasuk file activity, di navigasi, dan di menu dan string.xml
    2. Menambah dan mengubah ikon pada navbar
    
    3.

--- app/src/main/java/com/example/cloud_based_recyclable_household_waste_classification/ui/saved/SavedViewModel.kt
+++ app/src/main/java/com/example/cloud_based_recyclable_household_waste_classification/ui/saved/SavedViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.cloud_based_recyclable_household_waste_classification.ui.dashboard
+package com.example.cloud_based_recyclable_household_waste_classification.ui.saved
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class SavedViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is saved Fragment"
     }
     val text: LiveData<String> = _text
 }
