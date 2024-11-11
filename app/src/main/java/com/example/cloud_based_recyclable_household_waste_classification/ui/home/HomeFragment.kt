3051e6ad5daab96f124da1e0c7adc7fced79b84c
alvinsaputra123 <alvin.saputra20162016@gmail.com>
2024-11-12 02:15:50 +0700

    1. Menambahkan Izin di AndroidManifest.xml: Camera, Write & Read External Storage, dan Read Media Images
    2. Menambahkan permintaan perizinan Camera, Write & Read External Storage, dan Read Media Images
    
    3. Menambahkan PhotoPicker dan Intent Camera
    
    4. Menambahkan implementation (libs.transport.runtime)
    
    5. rename Id Button pada fragment_home.xml

--- app/src/main/java/com/example/cloud_based_recyclable_household_waste_classification/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/cloud_based_recyclable_household_waste_classification/ui/home/HomeFragment.kt
@@ -1,21 +1,75 @@
 package com.example.cloud_based_recyclable_household_waste_classification.ui.home
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.cloud_based_recyclable_household_waste_classification.databinding.FragmentHomeBinding
+import com.example.cloud_based_recyclable_household_waste_classification.ui.utils.getImageUri
 
 class HomeFragment : Fragment() {
 
+    companion object {
+        private const val REQUIRED_PERMISSION_CAMERA = Manifest.permission.CAMERA
+        private const val REQUIRED_PERMISSION_WRITE_EXTERNAL_STORAGE = Manifest.permission.WRITE_EXTERNAL_STORAGE
+        private const val REQUIRED_PERMISSION_READ_EXTERNAL_STORAGE = Manifest.permission.READ_EXTERNAL_STORAGE
+        private const val REQUIRED_PERMISSION_READ_MEDIA_IMAGES = Manifest.permission.READ_MEDIA_IMAGES
+    }
+
     private var _binding: FragmentHomeBinding? = null
+
+    private var currentImageUri: Uri? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            if (permissions[REQUIRED_PERMISSION_CAMERA] == true) {
+                Toast.makeText(requireContext(), "Camera permission granted", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(requireContext(), "Camera permission denied", Toast.LENGTH_LONG).show()
+            }
+
+            if (permissions[REQUIRED_PERMISSION_WRITE_EXTERNAL_STORAGE] == true || permissions[REQUIRED_PERMISSION_READ_EXTERNAL_STORAGE] == true) {
+                Toast.makeText(requireContext(), "Storage permission granted", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(requireContext(), "Storage permission denied", Toast.LENGTH_LONG).show()
+            }
+
+            if (permissions[REQUIRED_PERMISSION_READ_MEDIA_IMAGES] == true) {
+                Toast.makeText(requireContext(), "Read Media Images permission granted", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(requireContext(), "Read Media Images denied", Toast.LENGTH_LONG).show()
+            }
+        }
+
+    private fun allPermissionsGranted(): Boolean {
+        val cameraPermission = ContextCompat.checkSelfPermission(requireContext(), REQUIRED_PERMISSION_CAMERA) == PackageManager.PERMISSION_GRANTED
+        val storagePermission = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            true
+        } else {
+            ContextCompat.checkSelfPermission(requireContext(), REQUIRED_PERMISSION_WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED ||
+                    ContextCompat.checkSelfPermission(requireContext(), REQUIRED_PERMISSION_READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+        }
+        val readMediaImages = ContextCompat.checkSelfPermission(requireContext(), REQUIRED_PERMISSION_READ_MEDIA_IMAGES) == PackageManager.PERMISSION_GRANTED
+        return cameraPermission && storagePermission && readMediaImages
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,11 +82,57 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        if (!allPermissionsGranted()) {
+            requestPermissionLauncher.launch(
+                arrayOf(REQUIRED_PERMISSION_CAMERA, REQUIRED_PERMISSION_WRITE_EXTERNAL_STORAGE, REQUIRED_PERMISSION_READ_EXTERNAL_STORAGE, REQUIRED_PERMISSION_READ_MEDIA_IMAGES)
+            )
+        }
+
 //        val textView: TextView = binding.textHome
 //        homeViewModel.text.observe(viewLifecycleOwner) {
 //            textView.text = it
 //        }
+
+        binding.buttonGallery.setOnClickListener{
+            startGallery()
+        }
+
+        binding.buttonCamera.setOnClickListener{
+            startCamera()
+        }
+
         return root
+    }
+
+
+    private fun startGallery() {
+        launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri: Uri? ->
+        if (uri != null) {
+//            currentImageUri = uri
+//            showImage()
+
+        } else {
+            Log.d("Photo Picker", "No media selected")
+        }
+    }
+
+
+    private fun startCamera() {
+        currentImageUri = getImageUri(requireContext())
+        launcherIntentCamera.launch(currentImageUri)
+    }
+
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.TakePicture()
+    ) { isSuccess ->
+        if (isSuccess) {
+//            showImage()
+        }
     }
 
     override fun onDestroyView() {
